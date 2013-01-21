03e62cd70116f96c01cbafa969b7a89a33802ee4
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-01-21 13:12:42 -0800

    adds specs to scalers

--- spec/lib/heroku_resque_autoscaler/scaler_spec.rb
+++ spec/lib/heroku_resque_autoscaler/scaler_spec.rb
@@ -12,11 +12,28 @@ describe HerokuResqueAutoscaler do
   before :each do
     @heroku = mock(Heroku::API)
     HerokuResqueAutoscaler::Scaler.stub(:heroku).and_return(@heroku)
+
+    HerokuResqueAutoscaler.configure do |config|
+      config.heroku_api_key = "api-key"
+      config.heroku_app_name = "app-name"
+    end
   end
 
-  let(:heroku_app) {ENV['HEROKU_APP_NAME']}
+  let(:heroku_app_name) { "app-name" }
 
-  context "#workers" do
+  context ".api_key" do
+    it "returns api key" do
+      HerokuResqueAutoscaler::Scaler.api_key.should == "api-key"
+    end
+  end
+
+  context ".app_name" do
+    it "returns app name" do
+      HerokuResqueAutoscaler::Scaler.app_name.should == "app-name"
+    end
+  end
+
+  context ".workers" do
     it "returns the number of workers from the Heroku application" do
       Excon.stub(method: :get) do |params|
         { status: 200,
@@ -29,20 +46,20 @@ describe HerokuResqueAutoscaler do
       end
       connection = Excon.new("http://example.com", mock: true)
       response = connection.request(method: :get)
-      @heroku.should_receive(:get_ps).with(heroku_app).and_return(response)
+      @heroku.should_receive(:get_ps).with(heroku_app_name).and_return(response)
       HerokuResqueAutoscaler::Scaler.workers.should == 2
     end
   end
 
-  context "#workers=" do
+  context ".workers=" do
     it "sets the number of workers on Heroku to some quantity" do
       quantity = 10
-      @heroku.should_receive(:post_ps_scale).with(heroku_app, :worker, quantity)
+      @heroku.should_receive(:post_ps_scale).with(heroku_app_name, :worker, quantity)
       HerokuResqueAutoscaler::Scaler.workers = quantity
     end
   end
 
-  context "#job_count" do
+  context ".job_count" do
     it "returns the Resque job count" do
       num_pending = 10
       Resque.should_receive(:info).and_return({:pending => num_pending})
@@ -50,11 +67,19 @@ describe HerokuResqueAutoscaler do
     end
   end
 
-  context "#num_desired_heroku_workers" do
+  context ".working_job_count" do
+    it "returns the Resque working job count" do
+      num_working = 10
+      Resque.should_receive(:info).and_return({:working => num_working})
+      HerokuResqueAutoscaler::Scaler.working_job_count.should == num_working
+    end
+  end
+
+  context ".num_desired_heroku_workers" do
     it "returns the number of workers we should have (1 worker per x jobs)" do
       num_jobs = 100
       HerokuResqueAutoscaler::Scaler.stub(:job_count).and_return(num_jobs)
-      HerokuResqueAutoscalerTestClass.num_desired_heroku_workers.should == 5 
+      HerokuResqueAutoscalerTestClass.num_desired_heroku_workers.should == 5
 
       num_jobs = 38
       HerokuResqueAutoscaler::Scaler.unstub(:job_count)
@@ -64,7 +89,7 @@ describe HerokuResqueAutoscaler do
       num_jobs = 1
       HerokuResqueAutoscaler::Scaler.unstub(:job_count)
       HerokuResqueAutoscaler::Scaler.stub(:job_count).and_return(num_jobs)
-      HerokuResqueAutoscalerTestClass.num_desired_heroku_workers.should == 1 
+      HerokuResqueAutoscalerTestClass.num_desired_heroku_workers.should == 1
 
       num_jobs = 10000
       HerokuResqueAutoscaler::Scaler.unstub(:job_count)
@@ -73,7 +98,7 @@ describe HerokuResqueAutoscaler do
     end
   end
 
-  context "#after_perform_scale_down" do
+  context ".after_perform_scale_down" do
     it "scales down the workers to zero if there are no jobs pending" do
       HerokuResqueAutoscaler::Scaler.stub(:job_count).and_return(0)
       HerokuResqueAutoscaler::Scaler.stub(:workers).and_return(1)
@@ -89,7 +114,7 @@ describe HerokuResqueAutoscaler do
     end
   end
 
-  context "#after_enqueue_scale_up" do
+  context ".after_enqueue_scale_up" do
     it "ups the amount of workers if there are not enough" do
       num_workers = 5
       num_desired_workers = 6
